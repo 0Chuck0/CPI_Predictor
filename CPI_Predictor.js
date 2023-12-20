325f53b403402c84ff74f6aa459c1136556aec36
Saumya Shah <sshah89003@gmail.com>
2023-12-20 13:39:49 +0530

    Solved errors in calculation
    -->Added SPI
    -->Mentioned some terms

--- CPI_Predictor.js
+++ CPI_Predictor.js
@@ -2,7 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
     document.getElementById("add-course").addEventListener("click", addCourse);
     document.getElementById("remove-course").addEventListener("click", removeCourse);
-    document.getElementById("calculate").addEventListener("click", calculateCPI);
+    document.getElementById("calculate_cpi").addEventListener("click", calculateCPI);
+    document.getElementById("calculate_gp").addEventListener("click", calculateCPI_gp);
 
     let no_of_courses = 5;
     function validateFields()
@@ -51,6 +52,7 @@ document.addEventListener("DOMContentLoaded", function () {
             let credits = document.getElementById("total-credits").value;
 
             let prev_gradepts = credits * curr_cpi;
+            console.log(prev_gradepts);
 
             const expected_ptrs = document.getElementsByClassName("ptrs");
             const new_credits = document.getElementsByClassName("credits");
@@ -62,20 +64,103 @@ document.addEventListener("DOMContentLoaded", function () {
                 console.log(this_sem_credits);
                 
                 earned_gradepoints = parseFloat(earned_gradepoints) + (parseFloat(new_credits[i].value) * parseFloat(expected_ptrs[i].value));
+                console.log(earned_gradepoints);
 
             }
-            credits = this_sem_credits + credits;
-            let total_gradepoints = parseInt(earned_gradepoints) + parseInt(prev_gradepts);
+            credits = parseFloat(this_sem_credits) + parseFloat(credits);
+            console.log(credits);
+            let total_gradepoints = (earned_gradepoints) + (prev_gradepts);
+            console.log(total_gradepoints);
             let new_cpi = ((total_gradepoints) / parseFloat(credits));
 
             let spi = ((earned_gradepoints)/parseFloat(this_sem_credits));
             console.log(spi);
 
-            document.querySelector("#ans_header").innerHTML ="Your CPI after this Semeseter : " + new_cpi.toFixed(2) + "/nSPI : " + spi.toFixed(2);
+            document.querySelector("#ans_header").innerHTML ="CPI : " + new_cpi.toFixed(4) +"<br>" +"SPI : " + spi.toFixed(4);
             document.getElementById("ans_header").style.display="block";
         }
     }
 
+    function validateFields_gp()
+    {
+        // let cpi_vf = document.getElementById("cpi").value;
+        // if(cpi_vf == "" || cpi_vf > 10 || cpi_vf < 0)
+        // {
+        //     alert("Please Enter Valid CPI");
+        //     return false;
+        // }
+
+        let earned_gp_vf = document.getElementById("earned_gp").value;
+        if(earned_gp_vf == "" || earned_gp_vf < 0)
+        {
+            alert("Please Enter Valid Earned Gradepoints");
+            return false;
+        }
+        let total_credits = document.getElementById("total-credits").value;
+
+        if(total_credits =="" || total_credits < 0 )
+        {
+            alert("Please Enter Valid Total Credtis");
+        }
+
+        let ptrs_class = document.getElementsByClassName("ptrs");
+        
+        let credits_class = document.getElementsByClassName("credits");
+        for(let element of credits_class)
+        {
+            if(element.value == "" || element.value <2 || element.value > 5)
+            {
+                alert("Please Enter Valid Credits");
+                return false;
+            }
+        }
+
+        for(let element of ptrs_class)
+        {
+            if(element.value == "" || element.value < 0 || element.value > 10)
+            {
+                alert("Please Enter Valid Expected Pointers");
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    function calculateCPI_gp() {
+
+        if (validateFields_gp()) {
+            //let curr_cpi = document.getElementById("cpi").value;
+            let prev_gradepts = document.getElementById("earned_gp").value;
+            let credits = document.getElementById("total-credits").value;
+
+            console.log(prev_gradepts);
+
+            const expected_ptrs = document.getElementsByClassName("ptrs");
+            const new_credits = document.getElementsByClassName("credits");
+
+            let earned_gradepoints = 0;
+            let this_sem_credits = 0;
+            for (let i = 0; i <= no_of_courses - 1; i++) {
+                this_sem_credits = parseFloat(this_sem_credits) + parseFloat(new_credits[i].value);
+                console.log(this_sem_credits);
+                
+                earned_gradepoints = parseFloat(earned_gradepoints) + (parseFloat(new_credits[i].value) * parseFloat(expected_ptrs[i].value));
+                console.log(earned_gradepoints);
+
+            }
+            credits = parseFloat(this_sem_credits) + parseFloat(credits);
+            console.log(credits);
+            let total_gradepoints = parseFloat(earned_gradepoints) + parseFloat(prev_gradepts);
+            console.log(total_gradepoints);
+            let new_cpi = ((total_gradepoints) / parseFloat(credits));
+
+            let spi = ((earned_gradepoints)/parseFloat(this_sem_credits));
+
+            document.querySelector("#ans_header").innerHTML ="CPI : " + new_cpi.toFixed(4) +"<br>" +"SPI : " + spi.toFixed(4);
+            document.getElementById("ans_header").style.display="block";
+        }
+    }
 
     function addCourse() {
 
